06ecd4680d2a2261a5f1d3902e79ffb21770695f
Mimi <mireiag77@gmail>
2023-04-17 15:34:13 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_VT_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lista = new int[10];
+            randomNumber(lista);
+        }
+
+        static void randomNumber (int[] lista)
+        {
+            Random aleatorio = new Random();
+            for(int i = 0; i < lista.Length; i++)
+            {
+                lista[i] = aleatorio.Next(51);
+            }
+               printRandomNumber(lista);
+
+        }
+
+        static void printRandomNumber(int[] lista)
+        {
+            for(int i = 0; i < lista.Length; i++)
+            {
+                Console.WriteLine(lista[i]);
+
+            }
+
+        }
+    }
+}
